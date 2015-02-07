8820edf66639a477c28dd9a006c5452c456f9c7b
gisdaodao <asd1234>
2015-02-07 19:40:05 +0800

    ok

---
+++ ModernReader-master/Win8Reader/Reader/OKr.Win8Book.Client.Core/Context/MarkContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OKr.Common.Context;
+using OKr.Common.Storage;
+using OKr.Win8Book.Client.Core.Data;
+
+namespace OKr.Win8Book.Client.Core.Context
+{
+    public class MarkContext : OKrStateContextBase<Mark>
+    {
+        public MarkContext()
+            : base(new OKrStorage<Mark>(OKrBookConstant.MARK), true)
+        {
+ 
+        }
+
+
+        protected async override Task<Mark> DoLoad()
+        {
+            return new Mark();
+        }
+
+        protected override void DoSave(Mark data)
+        {
+        }
+    }
+}
---
+++ ModernReader-master/Win8Reader/Reader/OKr.Win8Book.Client.Core/Data/Progress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKr.Win8Book.Client.Core.Data
+{
+    public class Progress
+    {
+        public int Chapter { get; set; }
+        public int Page { get; set; }
+        public int Location { get; set; }
+        public string Percent { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
